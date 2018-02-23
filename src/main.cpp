1efbd116c71d02d61d1d07adec59266e36f59fa8
Student <student@ib.polsl.pl>
2018-02-23 11:04:38 +0100

    trzeci commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,35 @@
-#include <trygonometria.h>
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include <trygonometria.h>
+#else
+	#include <cmath>
+#endif
 #include <iostream>
-#include <trygonometria.h>
 
-int main() { 
-	double angle = 30;
-	std::cout<<degreemath::sin(angle)<<std::endl;
-	std::cout<<degreemath::cos(angle)<<std::endl;
-	std::cout<<degreemath::tan(angle)<<std::endl;
-	std::cout<<degreemath::ctg(angle)<<std::endl;
-	system("pause");
-	 }
+
+int main() 
+{ 
+	double sinus;
+	double cosinus;
+	double tangens;
+	double cotangens;
+	double pi = 3.14;
+
+	#ifdef USE_TRIGONOMETRY_DEGREE
+		sinus = sin_degree(45)
+		cosinus = cos_degree(45);
+		tangens = tan_degree(45);
+		cotangens = ctg_degree(45);
+	#else
+		sinus = sin(pi/4.0);
+		cosinus = cos(pi/4.0);
+		tangens = tan(pi/4.0);
+		cotangens = 1/tan(pi/4.0);
+	#endif
+	
+		std::cout<<sinus<<std::endl;
+		std::cout<<cosinus<<std::endl;
+		std::cout<<tangens<<std::endl;
+		std::cout<<cotangens<<std::endl;
+		system("pause");
+	 
+}
