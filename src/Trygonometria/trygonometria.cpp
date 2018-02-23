eeaf350d7a1ddfcd192959e12cf29e0342009199
Student <student@ib.polsl.pl>
2018-02-23 10:37:51 +0100

    drugi commit C

--- src/Trygonometria/trygonometria.cpp
+++ src/Trygonometria/trygonometria.cpp
@@ -1,23 +1,23 @@
 #include "trygonometria.h"
 #include <cmath>
 double degreemath::sin(double degree) {
- double radian = degree*3.14;
- return sin(radian);
+ double radian = (degree*3.14)/180;
+ return std::sin(radian);
 }
 
 double degreemath::cos(double degree) {
- double radian = degree*3.14;
- return cos(radian);
+ double radian = (degree*3.14)/180;
+ return std::cos(radian);
 }
 
 double degreemath::tan(double degree) {
- double radian = degree*3.14;
- return tan(radian);
+ double radian = (degree*3.14)/180;
+ return std::tan(radian);
 }
 
  double degreemath::ctg(double degree) {
- double radian = degree*3.14;
- return 1/tan(radian);
+ double radian = (degree*3.14)/180;
+ return 1/std::tan(radian);
 
 }
 //i tak dalej
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
-int main() { /*...*/ return 0; }
+#include <trygonometria.h>
+#include <iostream>
+#include <trygonometria.h>
+
+int main() { 
+	double angle = 30;
+	std::cout<<degreemath::sin(angle)<<std::endl;
+	std::cout<<degreemath::cos(angle)<<std::endl;
+	std::cout<<degreemath::tan(angle)<<std::endl;
+	std::cout<<degreemath::ctg(angle)<<std::endl;
+	system("pause");
+	 }
